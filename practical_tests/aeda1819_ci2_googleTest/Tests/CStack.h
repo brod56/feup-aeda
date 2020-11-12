aa90b4da824c7342a57ec67c3e26409005201845
Bruno Mendes <brod56@sapo.pt>
2020-11-12 18:47:31 +0000

    1920 ci2 wip

---
+++ practical_tests/aeda1819_ci2_googleTest/Tests/CStack.h
@@ -0,0 +1,66 @@
+// CStack.h - uma Stack simples de inteiros implementada com arrays
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class CStack { 
+    int top, d_size, *items;
+public:
+    CStack(int size) { top = -1; d_size = size; items = new int[size]; }
+    ~CStack() { delete []items;} 
+    bool empty() const { return top==-1; } 
+    int size() const { return top+1; } 
+    void push(int elem) { 
+        if(size()==d_size) cerr << "Stack Full!\n";
+        else { top++; items[top] = elem; }
+    }
+    void pop() { 
+        if(this->empty()) cerr << "Empty stack\n";
+        else top--;
+    }
+    int peek() const { 
+        if(this->empty()) cerr << "Empty stack\n";
+        else return items[top];
+        return 0;
+    }
+    string toStr() const {
+        stringstream oss;
+        for(int i = size()-1; i>=0; i--)
+        	oss << items[i] << " ";
+        return oss.str();
+    }
+    void print() const { 
+        cout << "Stack with " << size() << " elements: ";
+        for(int i = size()-1; i>=0; i--)
+            cout << items[i] << " ";
+        cout << endl;
+    }
+
+    void adicionaN(int n)
+	{  //Grupo 2 a)
+        int initialTop = peek();
+    	for (int current = 1; current <= n; ++current){
+    	    push(initialTop+current);
+    	}
+	}
+
+	bool inverte4() 
+	{   //Grupo 2 b)
+        if (size() < 4) return false;
+        vector<int> elements;
+        while (!empty()){
+            elements.push_back(peek());
+            pop();
+        }
+        for (int i = elements.size()-1; i > 3; --i){
+            push(elements.at(i));
+        }
+        for (int i = 0; i < 4; ++i){
+            push(elements.at(i));
+        }
+        return true;
+	}
+}; 
+
